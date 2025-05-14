00fab22010495e8b79360e3f1aa4ef8bf094fa4a
MoCo <gjkj330@gmail.com>
2025-05-14 12:37:44 +0900

    com

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -2,15 +2,7 @@ package com.yedam;
 
 public class JSExe {
 	public static void main(String[] args) {
-		test();
-
-		int sum = 0;
-		for (int i = 0; i <= 5; i++) {
-			int result = (int) (Math.random() * 10) + 1; // 1 <= x < 11 // 임의의 수 1 ~ 10사이의를 생성
-			sum += result;
-
-		}
-		System.out.println("결과 : " + sum);
+		test2();
 	} // end of main
 
 	public static void test() {
@@ -23,4 +15,14 @@ public class JSExe {
 		}
 		System.out.println("결과 : " + sum);
 	} // end of test
+	
+	public static void test2() {
+		int sum = 0;
+		for (int i = 0; i <= 5; i++) {
+			int result = (int) (Math.random() * 10) + 1; // 1 <= x < 11 // 임의의 수 1 ~ 10사이의를 생성
+			sum += result;
+
+		}
+		System.out.println("결과 : " + sum);
+	}
 } // end of class
