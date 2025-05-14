5270c1c5a31c66632426de9b2e1684bcaf792c62
MoCo <gjkj330@gmail.com>
2025-05-14 12:04:11 +0900

    Java_review

---
+++ HelloWorld/src/com/yedam/HelloJava.java
@@ -0,0 +1,23 @@
+package com.yedam;
+
+// 데이터타입이 중요 함
+// ctrl+shift+f => 정렬
+public class HelloJava {
+	public static void main(String[] args) {
+		// 데이터 타입 => int 정수 double 실수 boolean 참 거짓
+		int num1 = 10; // 초기화
+		double num2 = 20; // 자동형변환(promotion) 발생
+
+		// num1 = num2; // 큰 값을 작은 값에 넣으려면 에러가 남
+		num1 = (int) num2; // 강제형변환(casting)
+
+		int num3 = 100;
+		double num4 = 200;
+		double result = (double) num3 + num4;
+		System.out.println("결과는 " + result);
+
+		// 20 + 30 => 50
+//		System.out.println("결과는 " + 20 + 30); // 문자열로 출력 됨 
+		System.out.println("결과는 " + (20 + 30));
+	}
+}
--- HelloWorld/src/com/yedam/HelloWorld.java
+++ HelloWorld/src/com/yedam/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.yedam;
 	// 자바: 컴파일(****.java -> ****.class)
 
-public class HelloWorld {
+public class HelloWorld { // 관례상 클래스이름 첫글자를 대문자로 시작함
 	// main 메소드
   public static void main(String[] args) {
 	  for (int i=1; i<=3; i++) {
