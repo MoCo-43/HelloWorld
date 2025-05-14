499fd061b1be6e8baad176c3448cca42b200ea73
MoCo <gjkj330@gmail.com>
2025-05-14 12:57:45 +0900

    dd

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -1,9 +1,6 @@
 package com.yedam;
-<<<<<<< HEAD
 // Heaven JAVA
-=======
 // 자바 좋아
->>>>>>> branch 'master' of https://github.com/MoCo-43/HelloWorld.git
 public class JSExe {
 	public static void main(String[] args) {
 		test2();
