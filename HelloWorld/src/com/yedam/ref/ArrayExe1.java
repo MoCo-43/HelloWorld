0629859fc17492aff61c25c546c436a5c8246b2b
MoCo <gjkj330@gmail.com>
2025-05-16 17:52:15 +0900

    250516_lecture

--- HelloWorld/src/com/yedam/ref/ArrayExe1.java
+++ HelloWorld/src/com/yedam/ref/ArrayExe1.java
@@ -1,26 +1,83 @@
 package com.yedam.ref;
 
+// 교수님 gitHub link 250516까지 했는 코드 올림
+// 과제는 교재 p223 6번
+// github.com/changholee2010
 public class ArrayExe1 {
 	// field , method 선언문만 구현 가능
+
 	public static void main(String[] args) {
-		intAry();
+		ary_test();
+//		intAry();
 //		test3();
 //		test5();
 //		test6();
 //		test6_1();
-
+		
 	} // end of main
+
+    public static void ary_test() {
+    	// Math.random 활용해서 10 ~ 100 사이의 점수 생성
+    	// 학생 10명의 점수를 생성해서 학생점수의 평균을 구하고 최고점수가 몇점인지를 구하기
+    	// scoreAry 변수명
+
+    	int[] scoreAry = new int[10];
+    	
+    	for(int i=0; i<scoreAry.length; i++) {
+    	  int rand = (int) ((Math.random() * 90) + 10); 
+    	  scoreAry[i]=rand;
+    	}
+    	
+    	// 학생점수 평균
+		int sum = 0;
+		int cnt = 0;  // 횟수
+		for (int i = 0; i < scoreAry.length; i++) {
+			sum += scoreAry[i];
+			cnt++;
+			System.out.printf("i: %d 값: %d\n", i, scoreAry[i]);
+		}
+		double avg = 1.0 * sum / cnt;
+		System.out.printf("평균 : %.1f\n", avg);
+		
+		// 최고점수의 학생
+		int temp=0;
+		for(int j= 0; j < scoreAry.length -1; j++) {
+		  for (int i=0; i < scoreAry.length -1; i++) {	
+			  // 큰값기준으로 위치변경
+			  if(scoreAry[i] < scoreAry[i+1]) {
+				  temp = scoreAry[i];
+				  scoreAry[i] = scoreAry[i+1];
+				  scoreAry[i+1] = temp;
+			}
+		  }
+		}
+        for (int i = 0; i < scoreAry.length; i++) {
+        System.out.printf("최고점수 값: %d\n", scoreAry[i]);
+        }
+	    
+	    
+  }
 
 	public static void intAry() {
 		// int배열 => intAry : 10, 17, 22, 31, 55, 24
 		int[] intAry = { 10, 17, 22, 31, 55, 24 };
 
+
+    
+        
+		
+		
+		
 		// 합
 //		int sum = 0;
 //		for (int i = 0; i < intAry.length; i++) {
 //			sum += intAry[i];
 //			System.out.printf("sum: %d, i: %d 값: %d\n", sum, i, intAry[i]);
 //		}
+
+		
+		
+		
 		
 		// 합2
 		// 특정 인덱스 합을 구해 보기
@@ -31,6 +88,11 @@ public class ArrayExe1 {
 //			System.out.printf("sum: %d, i: %d 값: %d\n", sum, i, intAry[i]);
 //		}
 //	}
+		
+		
+		
+		
+		
 		// 합3
 		// 짝수인덱스만 합 구하기
 //		int sum = 0;
@@ -40,6 +102,8 @@ public class ArrayExe1 {
 //			System.out.printf("sum: %d, i: %d 값: %d\n", sum, i, intAry[i]);
 //		    }
 //		}
+
+		
 		
 		// 평균
 		// 짝수 인덱스 번호 평균 내기
@@ -54,6 +118,8 @@ public class ArrayExe1 {
 //		}
 //		double avg = 1.0 * sum / cnt;
 //		System.out.printf("평균 : %.1f\n", avg);
+
+		
 		
 		// 배열안 최고값 구하기
 //		int max = 0;
@@ -63,6 +129,8 @@ public class ArrayExe1 {
 //			}
 //		}
 //		System.out.printf("배열안 최고값 : %d", max);
+
+		
 		
 		// 배열의 위치 정렬
 		// 오름차순
@@ -80,7 +148,9 @@ public class ArrayExe1 {
 //        for (int i = 0; i < intAry.length; i++) {
 //        System.out.printf("i: %d 값: %d\n", i, intAry[i]);
 //        }
-        
+
+		
+		
         // 배열의 위치 정렬 2
         // 내림차순
 		int temp=0;
@@ -99,6 +169,10 @@ public class ArrayExe1 {
         }
 	}
 
+	
+	
+	
+	// 교재 P183 문제풀어보기
 	public static void test5() {
 		for (int i = 1; i <= 4; i++) {
 			for (int j = 1; j <= 4; j++) {
